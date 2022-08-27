4c57324dae553ef21f0dffd0fa0124d764bdc008
daulathussain <daulathussain92@gmail.com>
2022-08-27 13:07:39 +0530

    first commit

---
+++ components/componentsindex.js
@@ -0,0 +1,25 @@
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
+import HeroSection from "./HeroSection/HeroSection";
+import Service from "./Service/Service";
+import BigNFTSilder from "./BigNFTSilder/BigNFTSilder";
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
+
+export {
+  Button,
+  Footer,
+  NavBar,
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+};
---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/index.module.css";
+import {
+  HeroSection,
+  Service,
+  BigNFTSilder,
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+} from "../components/componentsindex";
+
+const Home = () => {
+  return (
+    <div className={Style.homePage}>
+      <HeroSection />
+      <Service />
+      <BigNFTSilder />
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
+      <Subscribe />
+    </div>
+  );
+};
+
+export default Home;
